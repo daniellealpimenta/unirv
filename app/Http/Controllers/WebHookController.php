9f1306513cbe0f4beeec9481bd4005bc517e2696
Daniel Leal Pimenta <129066504+daniellealpimenta@users.noreply.github.com>
2025-04-28 21:53:28 -0300

    Update WebHookController.php

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -4,30 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\HistoricoDeCompra;
+use MercadoPago\MercadoPagoConfig;
+use MercadoPago\Client\Payment\PaymentClient;
 
 class WebhookController extends Controller
 {
     public function handle(Request $request)
     {
-        $data = $request->all();
+        try {
+            $data = $request->all();
 
-        if (isset($data['data']['id'])) {
-            $paymentId = $data['data']['id'];
+            if (isset($data['data']['id'])) {
+                $paymentId = $data['data']['id'];
+                
+                MercadoPagoConfig::setAccessToken(env('MERCADO_PAGO_TOKEN'));
 
-            
-            $payment = (new \MercadoPago\Client\Payment\PaymentClient())->get($paymentId);
+                $paymentClient = new PaymentClient();
+                $payment = $paymentClient->get($paymentId);
 
-            
-            $historico = HistoricoDeCompra::where('payment_id', $paymentId)->first();
+                $historico = HistoricoDeCompra::where('payment_id', $paymentId)->first();
 
-            if ($historico) {
-                $historico->update([
-                    'status_do_pagamento' => $payment->status, 
-                    'comprovante_de_pagamento' => $payment->point_of_interaction->transaction_data->ticket_url ?? null, 
-                ]);
+                if ($historico) {
+                    $historico->update([
+                        'status_do_pagamento' => $payment->status,
+                        'comprovante_de_pagamento' => $payment->point_of_interaction->transaction_data->ticket_url ?? null,
+                    ]);
+                }
             }
-        }
 
-        return response()->json(['message' => 'Webhook recebido']);
+            return response()->json(['message' => 'Webhook recebido']);
+        } catch (\Exception $e) {
+            \Log::error('Erro no webhook: ' . $e->getMessage());
+            return response()->json(['error' => 'Falha no Webhook: ' . $e->getMessage()], 500);
+        }
     }
 }
